3b38d421bce9a5a98cba2c2eb2ca1a4e63a1c3b9
Blake <blakemealey@gmail.com>
2019-04-20 15:38:54 -0600

    Added jsdoc to ModalOutletService

--- projects/angular-modal-outlet/src/lib/services/modal-outlet/modal-outlet.service.ts
+++ projects/angular-modal-outlet/src/lib/services/modal-outlet/modal-outlet.service.ts
@@ -16,6 +16,14 @@ export class ModalOutletService {
     this.modalComponent$ = this.modalComponent.asObservable();
   }
 
+  /**
+   * Opens a modal containing a new component instance of the given component type, and populated
+   * with the given data. This will close any open modals before opening the next modal.
+   *
+   * @param componentType the class of the component to instantiate
+   * @param data the data to populate the component instance with
+   * @returns an observable which will send an event for the result of the dialog
+   */
   public showModal(componentType: any, data: any = null): Observable<ComponentModel> {
     // Close any existing modal without a result
     this.closeModal();
@@ -32,7 +40,12 @@ export class ModalOutletService {
     });
   }
 
-  public closeModal(result: any = null) {
+  /**
+   * Closes the current modal, if there is one
+   *
+   * @param result the result to pass to the subscriber of the modal observable
+   */
+  public closeModal(result: any = null): void {
     // Tell the modal component outlet to close the modal
     this.modalComponent.next(null);
 
